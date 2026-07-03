3ce0f07c89201d36da6c957e42d904a9578e8dad
halahabaybeh <hala.habaibe@gmail.com>
2026-07-03 22:49:55 +0300

    shopping list

---
+++ main.js
@@ -0,0 +1,69 @@
+const productForm = document.forms['productsForm'];
+
+let productslist = [];
+let indexEdited = null;
+
+const getProducts = () => {
+    if (localStorage.getItem('products') != null) {
+        productslist = JSON.parse(localStorage.getItem('products'));
+    }
+};
+
+
+productForm.onsubmit = (e) => {
+    e.preventDefault();
+    if (productForm.productName.value == '' || productForm.price.value == '' || productForm.quantity.value == '' || productForm.price.value <= 0 || productForm.quantity.value <= 0) {
+        alert('Please fill in all fields correctly.');
+        return;
+    }
+
+    const product = {
+        productName: productForm.productName.value,
+        price: productForm.price.value,
+        quantity: productForm.quantity.value,
+    }
+    if (indexEdited != null) {
+        productslist[indexEdited] = product;
+        indexEdited = null;
+    } else {
+        productslist.push(product);
+    }
+    localStorage.setItem('products', JSON.stringify(productslist));
+    productForm.reset();
+    console.log(productslist);
+    displayProducts();
+}
+
+const displayProducts = () => {
+    const items = productslist.map((item, index) => {
+        return `<tr>
+        <td>${index + 1}</td>
+        <td>${item.productName}</td>
+        <td>${item.price}</td>
+        <td>${item.quantity}</td>
+        <td>${(item.price * item.quantity)}</td>
+        <td><button onclick="deleteProducts(${index})">Delete</button></td>
+        <td><button onclick="editProducts(${index})">Edit</button></td>
+        </tr>`
+    });
+    document.querySelector('.productTableBody').innerHTML = items.join('');
+
+}
+
+const deleteProducts = (index) => {
+    productslist.splice(index, 1);
+    localStorage.setItem('products', JSON.stringify(productslist));
+    displayProducts();
+}
+
+const editProducts = (index) => {
+    const editeitem = productslist[index];
+    productForm.productName.value = editeitem.productName;
+    productForm.price.value = editeitem.price;
+    productForm.quantity.value = editeitem.quantity;
+    indexEdited = index;
+
+}
+
+getProducts();
+displayProducts();
